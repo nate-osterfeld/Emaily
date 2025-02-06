1903f2759b53f3d12732b8046ee095733b90b996
Nate Osterfeld <42900510+NateOsterfeld@users.noreply.github.com>
2025-02-06 01:45:38 -0500

    Add Google Strategy options

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy; // Creates instance of Google strategy
+const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy // Creates instance of Google strategy
+const keys = require('./config/keys.js')
 
-const app = express();
+const app = express()
 
 // Inform passport of strategy
-passport.use(new GoogleStrategy());
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback', // route user is directed to after granting permission (includes user's "code")
+		},
+		(accessToken) => {
+			console.log(accessToken)
+		},
+	),
+)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log('Server running on ' + PORT)
-});
+	console.log('Server running on ' + PORT)
+})
