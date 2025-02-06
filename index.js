ebc2719c63bfb97bd433071960b37c4bd96210f4
Nate Osterfeld <42900510+NateOsterfeld@users.noreply.github.com>
2025-02-06 01:00:58 -0500

    Passport setup

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express')
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // Creates instance of Google strategy
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'hello' })
-})
+// Inform passport of strategy
+passport.use(new GoogleStrategy());
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('Server running on ' + PORT)
-})
+});
