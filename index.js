6786802fc5b859cd2d054da4b8770b705f9af5b2
Nate Osterfeld <42900510+NateOsterfeld@users.noreply.github.com>
2025-02-06 02:34:16 -0500

    Setup callback function route handler

--- index.js
+++ index.js
@@ -7,24 +7,30 @@ const app = express()
 
 // Inform passport of strategy
 passport.use(
-	new GoogleStrategy(
+    new GoogleStrategy(
+        // Configures options for "/auth/google" route handler
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback', // route user is directed to after granting permission (includes user's "code" as query string)
-		},
+        },
+        // Verifies with callback function for "/auth/google/callback?code=<code>" route handler
 		(accessToken) => {
 			console.log(accessToken)
 		},
 	),
 )
 
+// Takes care of "options: StrategyOptions" config object (first argument of new GoogleStrategy)
 app.get(
 	'/auth/google', // route to enter into oauth flow
 	passport.authenticate('google', { // specifies the strategy based on the identifier existing in GoogleStrategy instance
 		scope: ['profile', 'email'], // specifies permissions
 	}),
 )
+
+// route to handle callback from Google after user grants permissions and we get their "code" AKA "verify" callback (2nd argument of new GoogleStrategy)
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
