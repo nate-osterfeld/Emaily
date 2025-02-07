dfdd022a895c5fcddccbfd866c46594031f9ae25
Nate Osterfeld <42900510+NateOsterfeld@users.noreply.github.com>
2025-02-06 23:34:00 -0500

    Wire Mongoose to MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose')
 require('./services/passport.js') // only need the file to run, don't need any variables imported
+const keys = require('./config/keys.js')
+
+mongoose.connect(keys.mongoURI)
 
 const app = express()
 
