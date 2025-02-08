73ea973b4a8153eab13df5b9572dd060b41319fc
Nate Osterfeld <42900510+NateOsterfeld@users.noreply.github.com>
2025-02-08 01:14:32 -0500

    Testing authentication

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,15 +2,20 @@ const passport = require('passport')
 
 // Export routes as arrow function to receive express app object
 module.exports = (app) => {
-    // Takes care of "options: StrategyOptions" config object (first argument of new GoogleStrategy)
-    app.get(
-        '/auth/google', // route to enter into oauth flow
-        passport.authenticate('google', {
-            // specifies the strategy based on the identifier existing in GoogleStrategy instance
-            scope: ['profile', 'email'], // specifies permissions
-        }),
-    )
-    
-    // route to handle callback from Google after user grants permissions and we get their "code" AKA "verify" callback (2nd argument of new GoogleStrategy)
-    app.get('/auth/google/callback', passport.authenticate('google'))   
+	// Takes care of "options: StrategyOptions" config object (first argument of new GoogleStrategy)
+	app.get(
+		'/auth/google', // route to enter into oauth flow
+		passport.authenticate('google', {
+			// specifies the strategy based on the identifier existing in GoogleStrategy instance
+			scope: ['profile', 'email'], // specifies permissions
+		}),
+	)
+
+	// route to handle callback from Google after user grants permissions and we get their "code" AKA "verify" callback (2nd argument of new GoogleStrategy)
+	app.get('/auth/google/callback', passport.authenticate('google'))
+
+    // We can now access our user object on the request object!!
+	app.get('/api/current_user', (req, res) => {
+		res.send(req.user)
+	})
 }
