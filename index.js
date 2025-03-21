5c216b2f430bdd866c6caaa9b0a19b3577dd8a96
Nate Osterfeld <osterfnl@mail.uc.edu>
2025-03-21 18:02:39 -0400

    Handling server side billing +
    use token 'id' to finalize charge using secret key for our Stripe account

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
+const bodyParser = require('body-parser')
 require('./models/User.js')
 require('./services/passport.js')
 const keys = require('./config/keys.js')
@@ -10,6 +11,7 @@ mongoose.connect(keys.mongoURI)
 
 const app = express()
 
+app.use(bodyParser.json())
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -19,8 +21,9 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
-// Run auth routes and pass it express app
+// Runs exported functions using express app
 require('./routes/authRoutes.js')(app)
+require('./routes/billingRoutes.js')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
