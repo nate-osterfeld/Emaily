f1b611edd54b3c0b6db758c9c47921b9056d36d3
Nate Osterfeld <osterfnl@mail.uc.edu>
2025-03-21 18:26:38 -0400

    Persist user's added credits to db +
    Add credits into User model
    Update credits on req.user object
    Save user object to DB
    Respond with user and new amount of credits

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,6 +2,10 @@ const keys = require('../config/keys.js')
 const stripe = require('stripe')(keys.stripeSecretKey)
 
 module.exports = (app) => {
+    if (!req.user) {
+        return res.status(401).send({ error: 'User must be logged in' })
+    }
+
     app.post('/api/stripe', async (req, res) => {
         const charge = await stripe.charges.create({
             amount: 500,
@@ -9,6 +13,11 @@ module.exports = (app) => {
             description: '$5 for 5 credits',
             source: req.body.id
         })
-        console.log('charge', charge)
+
+        // Add 5 credits to user (remember we are getting user off passport cookie-session which now includes credits in db as well)
+        req.user.credits += 5
+        const user = await req.user.save()
+
+        res.send(user)
     })
 }
