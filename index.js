f71a5d9acf7b66388b6aecf2d05e7f5aba3cf40a
Nate Osterfeld <osterfnl@mail.uc.edu>
2025-03-22 15:40:25 -0400

    Routing in production

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ app.use(passport.session())
 require('./routes/authRoutes.js')(app)
 require('./routes/billingRoutes.js')(app)
 
+if (process.env.NODE_ENV === 'production') {
+	// Serves all static files (main.js, main.css, etc.)
+	app.use(express.static('client/build'))
+
+	// Handles all routes for non-static requests by React Router (e.g. /surveys)
+	const path = require('path')
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
 	console.log('Server running on ' + PORT)
