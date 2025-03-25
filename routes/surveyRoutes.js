1418de322538a454dc1057f2be920858d629a54b
Nate Osterfeld <osterfnl@mail.uc.edu>
2025-03-25 16:53:11 -0400

    Create Survey instance

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,20 @@
+const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin.js')
 const requireCredits = require('../middlewares/requireCredits.js')
 
+const Survey = mongoose.model('surveys')
+
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+        const { title, subject, body, recipients } = req.body
 
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })), // returns an array of objects with k/v email: email (set to Recipient schema)
+            _user: req.user.id,
+            dateSent: Date.now()
+        })
     })
 }
